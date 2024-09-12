569168bc2b2438a9b87afaed20afadc0dc796596
AIMacGyver <robertannand@gmail.com>
2024-09-12 08:29:50 -0400

    refactor: fugative key mappings
    add descriptions

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,12 +1,11 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>gst', ':G status<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gaa', ':G add .<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gcmsg', ':G commit -m ', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gco', ':G checkout ', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gpl', ':G pull<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gps', ':G push<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gps', ':G push<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gs', ':G status<CR>', { desc = '[G]it [S]tatus', noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gaa', ':G add .<CR>', { desc = '[G]it [A]dd [A]ll', noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gcm', ':G commit -m ', { desc = '[G]it [C]ommit [M]essage', noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gco', ':G checkout ', { desc = '[G]it [C]heck [O]ut', noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gP', ':G pull<CR>', { desc = '[G]it [P]ull', noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gp', ':G push<CR>', { desc = '[G]it [p]ush', noremap = true, silent = true })
   end,
 }
