c248beafab92a7ce070a44be7382573577189c15
AIMacGyver <robertannand@gmail.com>
2024-09-26 14:40:46 -0400

    feat: add fugitive

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -7,5 +7,6 @@ return {
     vim.api.nvim_set_keymap('n', '<leader>gco', ':G checkout ', { desc = '[G]it [C]heck [O]ut', noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>gP', ':G pull<CR>', { desc = '[G]it [P]ull', noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>gp', ':G push<CR>', { desc = '[G]it [p]ush', noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gB', ':G blame', { desc = '[G]it [B]lame', noremap = true, silent = true })
   end,
 }
